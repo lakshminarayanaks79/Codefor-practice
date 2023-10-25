bfbdd2fe0f368c8eabf1237dfc2da21f2863fcd6
Lakshminarayana Kadechur <lakshminarayana.kadechur@bucherautomation.com>
2023-10-25 10:51:18 +0200

    more commits at home

--- main.cpp
+++ main.cpp
@@ -21,5 +21,19 @@ int main()
       std::cout << "Software Version is" << HELLOAPP_VERSION_MAJOR << "." << HELLOAPP_VERSION_MINOR<<std::endl;
     //std::cout << STD_CXX << std::endl;
 
+    #ifdef DEBUGINFO
+    std::cout << DEBUGINFO << std::endl;
+#endif
+    std::cout << "Software Version is" << HELLOAPP_VERSION_MAJOR << "." << HELLOAPP_VERSION_MINOR<<std::endl;
+    std::cout << sum(3,4);
+    WIFI_Init();
+    std::cout << "ProductType is" << ProductType << std::endl;
+    std::cout << "PRODUCT_YEAR is" << PRODUCT_YEAR << std::endl;
+
+    std::cout << "ProductType is 1" << ProductType << std::endl;
+    std::cout << "PRODUCT_YEAR is 1" << PRODUCT_YEAR << std::endl;
+
+      std::cout << "Software Version is" << HELLOAPP_VERSION_MAJOR << "." << HELLOAPP_VERSION_MINOR<<std::endl;
+
     return 0;
 }
